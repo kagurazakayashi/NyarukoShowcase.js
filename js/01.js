55255d2848e5962076febe1183bedc80d61574f2
0wew0 <wang124yi@163.com>
2018-04-20 15:04:55 +0800

    在图片下方添加标题文字

--- js/01.js
+++ js/01.js
@@ -11,7 +11,7 @@ var sparr0 = [];
 var sparr1 = [];
 var scrollimgs = [];
 var sparr2 = [];
-var scrollimgnum2 = false;
+var scrollimgnum2 = {"":[]};
 var scrollimgnum = {"":[]};
 var spw = 0;
 var spc = 10;
@@ -20,12 +20,16 @@ var spimgh = 0;
 var spnum = 0;
 var spshownum = 3;
 var spdivlen = Math.ceil(spnum/spshownum);
+var spith = 40;
 function setscrollimgs(imgsarr){
     scrollimgs = imgsarr;
 }
 function scrollpicture(spid) {
     var sparr00 = [];
+    var spshownum00 = true;//不足每页的需求
+    var spshownum01 = true;
     sparr[spid] = [];
+    spnum = 0;
     if(spdivlen < 3){
         spdivlen = 3;
     }
@@ -39,8 +43,9 @@ function scrollpicture(spid) {
     }
     var spc = 10;
     var spimgw = (spw - spc*(spshownum-1)) / spshownum;
-    var spimgh = spimgw / 16 * 9;
+    var spimgh = (spimgw / 16 * 9)+spith;
     scrollimgs.forEach(function(obj,i){
+        spnum++;
         var spcss = 'spDIV';
         if (spshownum == 3){
             if (i % spshownum == 1) {
@@ -52,19 +57,32 @@ function scrollpicture(spid) {
             }else{
                 spcss += ' sprightcss';
             }
+        } else if (spshownum == 1){
+            spdivlen = scrollimgs.length;
         }
-        sparr00.push("<div class='" + spcss + "' style = 'width: " + spimgw + "px;height: " + spimgh + "px;'><a href='" + obj[2] + "'><div></div><img src='" + obj[1] + "'/></a></div>");
+
+        sparr00.push("<div class='" + spcss + "' style='width: " + spimgw + "px;height: " + (spimgh+spith) + "px;'><div class='spimgdiv' style = 'width: " + spimgw + "px;height: " + spimgh + "px;'><a href='" + obj[2] + "'><img src='" + obj[1] + "'/></a></div><div class='scrollpicturetitle pictitle2'>" + obj[0] + "</div></div>");
         if (spshownum == 3){
             if (i % spshownum == 2){
                 var nowarr = sparr[spid];
-                nowarr.push(sparr00);
+                if (spshownum01){
+                    nowarr.unshift(sparr00);
+                    spshownum01 = false;
+                }else{
+                    nowarr.push(sparr00);
+                }
                 sparr[spid] = nowarr;
                 sparr00 = [];
             }
         }else if (spshownum == 2){
             if (i % spshownum == 1){
                 var nowarr = sparr[spid];
-                nowarr.push(sparr00);
+                if (spshownum01){
+                    nowarr.unshift(sparr00);
+                    spshownum01 = false;
+                }else{
+                    nowarr.push(sparr00);
+                }
                 sparr[spid] = nowarr;
                 sparr00 = [];
             }
@@ -74,7 +92,28 @@ function scrollpicture(spid) {
                 sparr00 = [];
             }
         }
-        spnum++;
+        if (spnum == scrollimgs.length && ((i % (spshownum-1)) < (spshownum - 1)) && spnum % spshownum != 0){
+            var nowarr = sparr[spid];
+            var fori = spshownum-(i % (spshownum-1));
+            for(var i = 0;i < fori;i++){
+                if (spshownum == 3){
+                    if (fori > 1 && i == 1) {
+                        spcss += ' spCentercss';
+                    }
+                }else if (spshownum == 2){
+                    spcss += ' sprightcss';
+                }
+                sparr00.push("<div class='" + spcss + "' style='width: " + spimgw + "px;height: " + (spimgh+spith) + "px;'><div class='spimgdiv' style = 'width: " + spimgw + "px;height: " + spimgh + "px;'></div></div>");
+            }
+            if (spshownum00){
+                nowarr.unshift(sparr00);
+                spshownum00 = false;
+            }else{
+                nowarr.push(sparr00);
+            }
+            sparr[spid] = nowarr;
+            sparr00 = [];
+        }
     });
     scrollimgnum[spid] = 1;
     // if(spshownum == 1){
@@ -87,33 +126,42 @@ function scrollpicture(spid) {
             var nowarr = sparr[spid];
             nowarr.push(sparr[spid][0],sparr[spid][0]);
             sparr[spid] = nowarr;
+            scrollimgnum2[spid] = false;
             break;
         case 2:
             var nowarr = sparr[spid];
             sparr2[spid] = nowarr;
-            scrollimgnum2 = true;
+            scrollimgnum2[spid] = true;
             nowarr.push(sparr[spid][0]);
             sparr[spid] = nowarr;
             break;
         default:
+        scrollimgnum2[spid] = false;
             break;
     }
+    
     sparr[spid].forEach(function(obj,i){
         $('#' + spid + ' .scrollpicture').append(sparr[spid][i]);
     });
     
     
     $('.sp').height($('.spDIV').height());
+    $('.scrollpicturetitle').width(spimgw);
+    $('.scrollpicturetitle').height(40);
     $('.scrollpicture').width(spdivlen * spw);
     $('.scrollpicture').height($('.spDIV').height());
     $(".scrollpicture").css({'left': -spw + 'px'});
-    $('.spDIV img').bind('load',function(){
-        if(this.height > $('.spDIV').height()){
-            $(this).css({'top': -(this.height - $('.spDIV').height())/2 + 'px'});
-        }else if(this.height < $('.spDIV').height()){
+    $('.spimgdiv img').bind('load',function(){
+        var imgwidth = this.width;
+        if (!imgwidth){
+            imgwidth = $('.spimgdiv').width();
+        }
+        if(this.height > $('.spimgdiv').height()){
+            $(this).css({'top': -(this.height - $('.spimgdiv').height())/2 + 'px'});
+        }else if(this.height < $('.spimgdiv').height()){
             $(this).width('auto');
-            $(this).height($('.spDIV').height());
-            $(this).css({'left': -(this.width - $('.spDIV').width())/2 + 'px'});
+            $(this).height($('.spimgdiv').height());
+            $(this).css({'left': -(this.width - $('.spimgdiv').width())/2 + 'px'});
         }
     });
     $(".spbutton").height($('.spDIV').height());
@@ -178,7 +226,7 @@ function rewh(){
                 case 2:
                     var nowarr = sparr[spid];
                     sparr2[spid] = nowarr;
-                    scrollimgnum2 = true;
+                    scrollimgnum2[spid] = true;
                     nowarr.push(sparr[spid][0]);
                     sparr[spid] = nowarr;
                     break;
@@ -195,7 +243,7 @@ function rewh(){
         });
     });
     var spimgw = (spw - spc*(spshownum-1)) / spshownum;
-    spimgh = spimgw / 16 * 9;
+    spimgh = spimgw / 16 * 9+spith;
     $(".spDIV").width(spimgw);
     $(".spDIV").height(spimgh);
     $('.sp').height($('.spDIV').height());
@@ -203,13 +251,13 @@ function rewh(){
     $('.scrollpicture').height($('.spDIV').height());
     $(".scrollpicture").css({'left': -spw + 'px'});
     // if (oldspshownum != spshownum) {
-        $('.spDIV img').each(function(){            
-            if(this.height > $('.spDIV').height()){
-                $(this).css({'top': -(this.height - $('.spDIV').height())/2 + 'px'});
-            }else if(this.height < $('.spDIV').height()){
+        $('.spimgdiv img').each(function(){            
+            if(this.height > $('.spimgdiv').height()){
+                $(this).css({'top': -(this.height - $('.spimgdiv').height())/2 + 'px'});
+            }else if(this.height < $('.spimgdiv').height()){
                 $(this).width('auto');
-                $(this).height($('.spDIV').height());
-                $(this).css({'left': -(this.width - $('.spDIV').width())/2 + 'px'});
+                $(this).height($('.spimgdiv').height());
+                $(this).css({'left': -(this.width - $('.spimgdiv').width())/2 + 'px'});
             }
         });
     // }
@@ -222,7 +270,7 @@ function leftbutton(spid){
         $('.scrollpicture').css({'left': -$('body').width() + 'px'});
         $('#' + spid + ' .scrollpicture').html("");
         sparr[spid].forEach(function(obj, i){
-            if (scrollimgnum2){
+            if (scrollimgnum2[spid]){
                 sparr1.push(sparr2[spid][scrollimgnum[spid]]);
                 if(scrollimgnum[spid] == 0){
                     scrollimgnum[spid]++;
@@ -241,18 +289,18 @@ function leftbutton(spid){
             $('#' + spid + ' .scrollpicture').append(obj);
         });
         // rewh();
-        $('.spDIV img').each(function(){
-            if(this.height > $('.spDIV').height()){
-                $(this).css({'top': -(this.height - $('.spDIV').height())/2 + 'px'});
-            }else if(this.height < $('.spDIV').height()){
+        $('.spimgdiv img').each(function(){
+            if(this.height > $('.spimgdiv').height()){
+                $(this).css({'top': -(this.height - $('.spimgdiv').height())/2 + 'px'});
+            }else if(this.height < $('.spimgdiv').height()){
                 $(this).width('auto');
-                $(this).height($('.spDIV').height());
+                $(this).height($('.spimgdiv').height());
                 spw = $('body').width();
                 var spimgw = (spw - spc*(spshownum-1)) / spshownum;
-                spimgh = spimgw / 16 * 9;
-                $(".spDIV").height(spimgh);
-                $(".spDIV").width(spimgw);
-                $(this).css({'left': -($(this).width - $('.spDIV').width())/2 + 'px'});
+                spimgh = spimgw / 16 * 9 + spith;
+                $(".spimgdiv").height(spimgh);
+                $(".spimgdiv").width(spimgw);
+                $(this).css({'left': -($(this).width - $('.spimgdiv').width())/2 + 'px'});
             }
         });
         sparr[spid] = sparr1;
@@ -270,7 +318,7 @@ function rightbutton(spid){
         $('.scrollpicture').css({'left': -$('body').width() + 'px'});
         $('#' + spid + ' .scrollpicture').html("");
         sparr[spid].forEach(function(obj, i){
-            if (scrollimgnum2){
+            if (scrollimgnum2[spid]){
                 sparr1.push(sparr2[spid][scrollimgnum[spid]]);
                 if(scrollimgnum[spid] == 0){
                     scrollimgnum[spid]++;
@@ -288,18 +336,18 @@ function rightbutton(spid){
         sparr1.forEach(function(obj,i){
             $('#' + spid + ' .scrollpicture').append(obj);
         });
-        $('.spDIV img').each(function(){
-            if(this.height > $('.spDIV').height()){
-                $(this).css({'top': -(this.height - $('.spDIV').height())/2 + 'px'});
-            }else if(this.height < $('.spDIV').height()){
+        $('.spimgdiv img').each(function(){
+            if(this.height > $('.spimgdiv').height()){
+                $(this).css({'top': -(this.height - $('.spimgdiv').height())/2 + 'px'});
+            }else if(this.height < $('.spimgdiv').height()){
                 $(this).width('auto');
-                $(this).height($('.spDIV').height());
+                $(this).height($('.spimgdiv').height());
                 spw = $('body').width();
                 var spimgw = (spw - spc*(spshownum-1)) / spshownum;
-                spimgh = spimgw / 16 * 9;
-                $(".spDIV").height(spimgh);
-                $(".spDIV").width(spimgw);
-                $(this).css({'left': -(this.width - $('.spDIV').width())/2 + 'px'});
+                spimgh = spimgw / 16 * 9 + spith;
+                $(".spimgdiv").height(spimgh);
+                $(".spimgdiv").width(spimgw);
+                $(this).css({'left': -(this.width - $('.spimgdiv').width())/2 + 'px'});
             }
         });
         sparr[spid] = sparr1;
